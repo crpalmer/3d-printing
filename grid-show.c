d984b31da091d55ef609fa1cd588e54324768fcb
Christopher R. Palmer <crpalmer@gmail.com>
2018-03-06 20:48:13 -0500

    grid-show: Fix orientation, show probed points, csv is csv

--- grid-show.c
+++ grid-show.c
@@ -2,41 +2,82 @@
 #include <stdlib.h>
 #include <string.h>
 #include <float.h>
+#include <math.h>
+
+float array[1024*1024];
 
 int main(int argc, char **argv)
 {
-     unsigned size = fgetc(stdin);
+     int size = fgetc(stdin);
      float radius;
      int x, y;
-     float small = FLT_MAX;
-     float big = -FLT_MAX;
+     float small = FLT_MAX, circle_small = FLT_MAX;
+     float big = -FLT_MAX, circle_big = -FLT_MAX;
      int csv_mode = 0;
+     int circle_mode = 0;
+     int zero_mode = 0;
+     float d;
 
-     if (argc > 1) {
-	if (argc == 2 && strcmp(argv[1], "--csv") == 0) csv_mode = 1;
+     while (argc > 1) {
+	if (argc >= 2 && strcmp(argv[1], "--csv") == 0) csv_mode = 1;
+	else if (argc >= 2 && strcmp(argv[1], "--circle") == 0) circle_mode = 1;
+	else if (argc >= 2 && strcmp(argv[1], "--zero") == 0) zero_mode = 1;
 	else {
-	     fprintf(stderr, "usage: [--csv]\n");
+	     fprintf(stderr, "usage: [--csv] | [--circle]\n");
 	     exit(1);
 	}
+	argc--;
+	argv++;
      }
 
      fread(&radius, sizeof(radius), 1, stdin);
-     fprintf(stderr, "%dx%d grid with radius %f\n", size, size, radius);
+     d = 2*radius / (size-1);
+     fprintf(stderr, "%dx%d grid with radius %f (point distance %f)\n", size, size, radius, d);
 
-     for (x = 0; x < size; x++) {
-	for (y = 0; y < size; y++) {
-	    float v;
+     for (y = 0; y < size; y++) {
+	for (x = 0; x < size; x++) {
+	    fread(&array[x+y*size], sizeof(float), 1, stdin);
+	}
+     }
 
-	    fread(&v, sizeof(v), 1, stdin);
+     if (zero_mode) {
+	float v0 = array[size/2 + (size/2*size)];
 
-	    if (csv_mode) printf("%d %d %f\n", x, y, v);
-	    else printf("%8.4f", v);
+	for (x = 0; x < size; x++) {
+	    for (y = 0; y < size; y++) {
+		array[x + y*size] -= v0;
+	    }
+	}
+    }
+
+    /* print in the order matching looking at the bed from the front (-x to left, -y to front) */
+    for (y = size-1; y >= 0; y--) {
+	int need_comma = 0;
+	float Y = (y*d) - radius;
+
+	for (x = 0; x < size; x++) {
+	    float X = (x*d) - radius;
+	    int report_it;
+	    float v=array[x + y*size];
+
+	    report_it = !circle_mode || (sqrt(X*X+Y*Y) <= radius);
+
+	    if (csv_mode) {
+		if (need_comma) printf(",");
+		if (report_it) printf("%.4f", v);
+		need_comma = 1;
+	    } else {
+		if (report_it) printf("%8.2f", v);
+		else printf("%8c", ' ');
+	    }
 
 	    if (v > big) big = v;
 	    if (v < small) small = v;
+	    if (v > circle_big && report_it) circle_big = v;
+	    if (v < circle_small && report_it) circle_small = v;
 	}
-	if (! csv_mode) printf("\n");
+	printf("\n");
     }
-    fprintf(stderr, "range of values: %f..%f\n", small, big);
+    fprintf(stderr, "range of values: %.4f..%.4f (probed %.4f..%.4f)\n", small, big, circle_small, circle_big);
 }
 
