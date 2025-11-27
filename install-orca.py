8a9141e1dde41acf0d5b8722b3893f7e81732b2b
Christopher R. Palmer <crpalmer@gmail.com>
2025-11-27 12:44:39 -0500

    orca: build a list of compatible_printers based on notes

--- install-orca.py
+++ install-orca.py
@@ -49,14 +49,30 @@ def write_json(dest, config):
         config["version"] = version
         json.dump(config, f, indent=4)
 
+printer_notes = {}
+
 def write_config(config, subsystem):
     name = get_name(config)
     config = set_name(config, name, subsystem)
+    if "printer_notes" in config:
+        printer_notes[name] = config["printer_notes"]
+    if "compatible_printers_condition" in config:
+        config["compatible_printers"] = ""
+        config["compatible_printers"] = []
+        for printer in printer_notes.keys():
+            satisfied = True
+            notes = printer_notes[printer]
+            for condition in config["compatible_printers_condition"].split("\n"):
+                if condition not in notes:
+                    satisfied = False
+                    break
+            if satisfied:
+                config["compatible_printers"].append(printer)
+        config["compatible_printers_condition"] = ""
     write_json(orca_dir + "/" + subsystem + "/" + name + ".json", config)
 
 def write_base(config, subsystem, name):
     config = set_name(config, name, subsystem)
-    config["inherits"] = ""
     write_json(orca_dir + "/" + subsystem + "/base/" + name + ".json", config)
 
 def read_json(path):
@@ -69,10 +85,12 @@ def combine_json(config1, config2):
     for key in config1.keys():
         config[key] = config1[key]
     for key in config2.keys():
-        if key == "compatible_printers_condition" and key in config and key in config2 and config[key].strip() != '':
-            config[key] = "(" + config[key] + ") and (" + config2[key] + ")"
-        elif key == "name" and key in config and key in config2:
+        if key == "compatible_printers_condition" and key in config:
+            config[key] += config2[key]
+        elif key == "name" and key in config:
             config["name"] += config2["name"]
+        elif key == "printer_notes" and key in config:
+            config["printer_notes"] += config2["printer_notes"]
         else:
             config[key] = config2[key]
     return config
@@ -97,7 +115,13 @@ def apply_modifiers_to_dir(path, subsystem):
     modifiers = read_json(path + "/modifiers.json")
     base = read_json(path + "/base.json")
     write_base(base, subsystem, base["name"])
+
     base_config = { "inherits": base["name"] }
+    if "printer_notes" in base:
+        base_config["printer_notes"] = base["printer_notes"]
+    if "compatible_printers_condition" in base:
+        base_config["compatible_printers_condition"] = base["compatible_printers_condition"]
+
     for chain in modifiers["chains"]:
         apply_chain(base_config, path, subsystem, chain)
         
@@ -118,6 +142,6 @@ def process(path, subsystem, name):
             else:
                 write_config(config, subsystem)
 
-for subsystem in [ "filament", "machine", "process" ]:
+for subsystem in [ "machine", "filament", "process" ]:
     mkdir_recursive(orca_dir + "/" + subsystem + "/base")
     process("orca/" + subsystem, subsystem, None)
